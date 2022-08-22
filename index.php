c7f2a6e8c98dcc462906a77e33dce1eab4541594
NotUser5 <jmtorrichelli@gmail.com>
2022-08-22 15:22:06 -0300

    att

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <link rel="script" href="script.js">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello Word</h1>
-    <?php
-        echo "Teste";
-    ?>
-</body>
-</html>
+<link rel="stylesheet" href="style.css">
+<link rel="script" href="script.js">
+
+<?php
+    #Comentario
+    //Comentario
+    /*
+    Bloco de Comentario
+    */
+
+    echo "Teste\n";
+    echo "<br>";
+    echo "<h1>Hello Word</h1>";
+    
+    $nome = "João";
+    $idade = 19;
+    $altura = 1.80;
+    $solteiro = true;
+
+    $prova1 = 8;
+    $prova2 = 7.5;
+    $soma = $prova1 + $prova2;
+    $media = $soma / 2;
+    
+    $multiplicação = $prova1 * $prova2;
+    $subtração = $prova1 - $prova2;
+
+    CONST NOMECOMPLETO = "JOÃO MARCOS TORRICHELLI MATTOS";
+
+    echo NOMECOMPLETO;
+    echo "<br>";
+    echo $media;
+    echo "<br>";
+    echo $multiplicação;
+    echo "<br>";
+    echo $subtração;
+    echo "<br>";
+?>
